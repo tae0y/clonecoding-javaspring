5dd472fbc0f9edfd135dd63470bd9b578b1e1696
장서윤 <hi.seoyoon22@gmail.com>
2023-04-10 09:11:59 +0100

    karrot-0006: Making sample test code using the Junit #13

---
+++ src/main/java/com/karrot/App.java
@@ -0,0 +1,11 @@
+package com.karrot;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
