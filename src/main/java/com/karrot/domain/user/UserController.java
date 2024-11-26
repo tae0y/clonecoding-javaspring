de474858bf4a3cd20817a5f92435a72d8e8ad834
tae0y <tae0yp@gmail.com>
2024-11-27 00:43:18 +0900

    update user controller - CRUD 메서드 추가

--- src/main/java/com/karrot/domain/user/UserController.java
+++ src/main/java/com/karrot/domain/user/UserController.java
@@ -1,12 +1,75 @@
 package com.karrot.domain.user;
 
+import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@Api(tags = "User", description = "사용자 정보")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Operation(summary ="Post User", description = "Making new User")
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        throw new NotYetImplementedException();
+        //HttpStatus status = HttpStatus.CREATED;
+        //User saved = userService.save(user);
+        //return new ResponseEntity<>(saved, status);
+    }
+
+    @Operation(summary ="Get User", description = "Return all Users")
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        throw new NotYetImplementedException();
+        //return userService.getAllUsers();
+    }
+
+    @Operation(summary ="Get User", description = "Return User by ID")
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        throw new NotYetImplementedException();
+        //return userService.getUser(id)
+        //        .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+        //        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Operation(summary ="Put User", description = "Update User by ID")
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        throw new NotYetImplementedException();
+        //return userService.getUser(id)
+        //        .map(existingUser -> {
+        //            user.setId(existingUser.getId());
+        //            User saved = userService.save(user);
+        //            return new ResponseEntity<>(saved, HttpStatus.OK);
+        //        })
+        //        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Operation(summary ="Delete User", description = "Delete User by ID")
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        throw new NotYetImplementedException();
+        //return userService.getUser(id)
+        //        .map(user -> {
+        //            userService.delete(user);
+        //            return new ResponseEntity<>(HttpStatus.OK);
+        //        })
+        //        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
