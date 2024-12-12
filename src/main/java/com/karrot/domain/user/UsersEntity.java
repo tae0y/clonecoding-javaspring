3adfd635841ade69f1fdca6a90682c742a511e7b
tae0y <tae0yp@gmail.com>
2024-12-12 14:30:59 +0900

    add : response, request DTO 추가

---
+++ src/main/java/com/karrot/domain/user/UsersEntity.java
@@ -0,0 +1,114 @@
+package com.karrot.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.lang.Nullable;
+
+import lombok.Data;
+
+@Entity
+@Table(name="users")
+@Data
+public class UsersEntity {
+    /**
+     * 사용자 ID
+     */
+    @Id
+    //TODO : [개선] H2, PostgreSQL 등 각 DB에 맞는 ID 생성 전략을 수립하여야함
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    @NotNull
+    private Long id;
+
+    /**
+     * 사용자 이름
+     */
+    @Column(nullable = false, length = 10)
+    @NotNull
+    @Size(max = 10)
+    private String name;
+
+    /**
+     * 사용자 소개글
+     */
+    @Column(nullable = true, length = 255)
+    @Nullable
+    @Size(max = 255)
+    private String introduction;
+
+    /**
+     * 사용자 활성화여부
+     */
+    @Column(nullable = false)
+    @NotNull
+    private boolean isActive;
+
+    /**
+     * 사용자 등록 프로그램 Id(자체 회원가입, OAuth 등)
+     */
+    @Column(nullable = false, length = 20)
+    @NotNull
+    @Size(max = 20)
+    //@CreatedBy TODO: [개선] JPA Auditing을 사용하여 사용자 ID를 자동으로 생성하도록 수정, 그럼 시스템단의 변경은?
+    private String createProgramId;
+
+    /**
+     * 사용자 등록일시
+     */
+    @Column(nullable = false)
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    /**
+     * 사용자 수정 프로그램 Id(자체 프로필수정, 데이터연동 등)
+     */
+    @Column(nullable = true, length = 20)
+    @Nullable
+    @Size(max = 20)
+    //@LastModifiedBy TODO: [개선] JPA Auditing을 사용하여 사용자 ID를 자동으로 생성하도록 수정, 그럼 시스템단의 변경은?
+    private String modifyProgramId;
+
+    /**
+     * 사용자 수정일시
+     */
+    @Column(nullable = true)
+    @Nullable
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+
+    /** 
+     * 사용자 삭제 프로그램 Id(자체 회원탈퇴, 관리자 삭제, 개인정보 보존만료 등)
+     */
+    @Column(nullable = true, length = 20)
+    @Nullable
+    @Size(max = 20)
+    private String deleteProgramId;
+
+    /**
+     * 사용자 삭제일시
+     */
+    @Column(nullable = true)
+    @Nullable
+    private LocalDateTime deletedDateTime;
+
+    /**
+     * 삭제일시 자동 생성
+     */
+    @PreRemove
+    public void preRemove() {
+        this.deletedDateTime = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/karrot/domain/user/UsersResponseDTO.java
@@ -0,0 +1,40 @@
+package com.karrot.domain.user;
+
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(name = "User", description = "사용자 정보")
+public class UsersResponseDTO {
+    @Schema(name = "id", description = "사용자 ID", required = true, nullable = false, example = "1")
+    private Long id;
+
+    @Schema(name = "name", description = "사용자 이름", required = false, nullable = false, example = "John Doe", maxLength = 10)
+    private String name;
+
+    @Schema(name = "introduction", description = "사용자 소개글", required = false, nullable = true, example = "Hello, I am John.", maxLength = 255)
+    private String introduction;
+
+    @Schema(name = "isActive", description = "사용자 활성화여부", required = false, nullable = false, example = "true")
+    private boolean isActive;
+
+    @Schema(name = "createProgramId", description = "사용자 등록 프로그램 Id(자체 회원가입, OAuth 등)", required = false, nullable = false, example = "oauth", maxLength = 20)
+    private String createProgramId;
+
+    @Schema(name = "createdDateTime", description = "사용자 등록일시", required = false, nullable = false, example = "2023-01-01T12:00:00")
+    private LocalDateTime createdDateTime;
+
+    @Schema(name = "modifyProgramId", description = "사용자 수정 프로그램 Id(자체 프로필수정, 데이터연동 등)", required = true, nullable = false, example = "profile_edit", maxLength = 20)
+    private String modifyProgramId;
+
+    @Schema(name = "modifiedDateTime", description = "사용자 수정일시", required = true, nullable = false, example = "2023-01-02T12:00:00")
+    private LocalDateTime modifiedDateTime;
+
+    @Schema(name = "deleteProgramId", description = "사용자 삭제 프로그램 Id(자체 회원탈퇴, 관리자 삭제, 개인정보 보존만료 등)", required = false, nullable = true, example = "admin_delete", maxLength = 20)
+    private String deleteProgramId;
+
+    @Schema(name = "deletedDateTime", description = "사용자 삭제일시", required = false, nullable = true, example = "2023-01-03T12:00:00")
+    private LocalDateTime deletedDateTime;
+}
