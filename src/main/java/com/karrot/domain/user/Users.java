c4c1e8d5769337a2092fc0764c52118fad4256fa
tae0y <tae0yp@gmail.com>
2024-12-12 13:49:25 +0900

    update : Users 필드별 예시 추가

--- src/main/java/com/karrot/domain/user/Users.java
+++ src/main/java/com/karrot/domain/user/Users.java
@@ -2,14 +2,13 @@ package com.karrot.domain.user;
 
 import java.time.LocalDateTime;
 
-import javax.annotation.Generated;
-import javax.annotation.PreDestroy;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
@@ -23,43 +22,43 @@ import lombok.Data;
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(name = "id", description = "사용자 ID", required = true, nullable = false)
+    @Schema(name = "id", description = "사용자 ID", required = true, nullable = false, example = "1")
     @Column(nullable = false)
     private Long id;
 
-    @Schema(name = "name", description = "사용자 이름", required = false, nullable = false)
+    @Schema(name = "name", description = "사용자 이름", required = false, nullable = false, example = "John Doe")
     @Column(nullable = false)
     private String name;
 
-    @Schema(name = "introduction", description = "사용자 소개글", required = false, nullable = true)
+    @Schema(name = "introduction", description = "사용자 소개글", required = false, nullable = true, example = "Hello, I am John.")
     @Column(nullable = true)
     private String introduction;
 
-    @Schema(name = "isActive", description = "사용자 활성화여부", required = false, nullable = false)
+    @Schema(name = "isActive", description = "사용자 활성화여부", required = false, nullable = false, example = "true")
     @Column(nullable = false)
     private boolean isActive;
 
-    @Schema(name = "createProgramId", description = "사용자 등록 프로그램 Id(자체 회원가입, OAuth 등)", required = false, nullable = false)
+    @Schema(name = "createProgramId", description = "사용자 등록 프로그램 Id(자체 회원가입, OAuth 등)", required = false, nullable = false, example = "oauth")
     @Column(nullable = false)
     private String createProgramId;
 
-    @Schema(name = "createdDateTime", description = "상품 등록일시", required = false, nullable = false)
+    @Schema(name = "createdDateTime", description = "사용자 등록일시", required = false, nullable = false, example = "2023-01-01T12:00:00")
     @Column(nullable = false)
     private LocalDateTime createdDateTime;
 
-    @Schema(name = "modifyProgramId", description = "사용자 수정 프로그램 Id(자체 프로필수정, 데이터연동 등)", required = true, nullable = false)
+    @Schema(name = "modifyProgramId", description = "사용자 수정 프로그램 Id(자체 프로필수정, 데이터연동 등)", required = true, nullable = false, example = "profile_edit")
     @Column(nullable = false)
     private String modifyProgramId;
 
-    @Schema(name = "modifiedDateTime", description = "상품 수정일시", required = true, nullable = false)
+    @Schema(name = "modifiedDateTime", description = "사용자 수정일시", required = true, nullable = false, example = "2023-01-02T12:00:00")
     @Column(nullable = false)
     private LocalDateTime modifiedDateTime;
 
-    @Schema(name = "deleteProgramId", description = "사용자 삭제 프로그램 Id(자체 회원탈퇴, 관리자 삭제, 개인정보 보존만료 등)", required = false, nullable = true)
+    @Schema(name = "deleteProgramId", description = "사용자 삭제 프로그램 Id(자체 회원탈퇴, 관리자 삭제, 개인정보 보존만료 등)", required = false, nullable = true, example = "admin_delete")
     @Column(nullable = true)
     private String deleteProgramId;
 
-    @Schema(name = "deletedDateTime", description = "상품 삭제일시", required = false, nullable = true)
+    @Schema(name = "deletedDateTime", description = "사용자 삭제일시", required = false, nullable = true, example = "2023-01-03T12:00:00")
     @Column(nullable = true)
     private LocalDateTime deletedDateTime;
 
@@ -74,8 +73,8 @@ public class Users {
         this.modifiedDateTime = LocalDateTime.now();
     }
 
-    @PreDestroy
-    public void preDestroy() {
+    @PreRemove
+    public void preRemove() {
         this.deletedDateTime = LocalDateTime.now();
     }
 }
