c2c3e7f39ff8c3bad976f257fc741aec5192a49f
tae0y <tae0yp@gmail.com>
2024-12-28 21:47:16 +0900

    update : dto swagger 수정

--- src/main/java/com/karrot/domain/user/UsersRequestDTO.java
+++ src/main/java/com/karrot/domain/user/UsersRequestDTO.java
@@ -4,6 +4,8 @@ import javax.validation.constraints.Size;
 
 import org.springframework.lang.Nullable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -15,6 +17,7 @@ public class UsersRequestDTO {
      */
     @Schema(name = "name", description = "사용자 이름", required = false, nullable = false, example = "John Doe", maxLength = 10)
     @Size(max = 10)
+    @JsonProperty("name")
     private String name;
 
     /**
@@ -22,12 +25,14 @@ public class UsersRequestDTO {
      */
     @Schema(name = "introduction", description = "사용자 소개글", required = false, nullable = true, example = "Hello, I am John.", maxLength = 255)
     @Size(max = 255)
+    @JsonProperty("introduction")
     private String introduction;
 
     /**
      * 사용자 활성화여부
      */
     @Schema(name = "isActive", description = "사용자 활성화여부", required = false, nullable = false, example = "true")
+    @JsonProperty("isActive")
     private boolean isActive;
 
     /**
@@ -35,6 +40,7 @@ public class UsersRequestDTO {
      */
     @Schema(name = "createProgramId", description = "사용자 등록 프로그램 Id(자체 회원가입, OAuth 등)", required = false, nullable = true, example = "oauth", maxLength = 20)
     @Size(max = 20)
+    @JsonProperty("createProgramId")
     private String createProgramId;
 
     /**
@@ -43,6 +49,7 @@ public class UsersRequestDTO {
     @Schema(name = "modifyProgramId", description = "사용자 수정 프로그램 Id(자체 프로필수정, 데이터연동 등)", required = true, nullable = true, example = "profile_edit", maxLength = 20)
     @Nullable
     @Size(max = 20)
+    @JsonProperty("modifyProgramId")
     private String modifyProgramId;
 
     /**
@@ -50,5 +57,12 @@ public class UsersRequestDTO {
      */
     @Schema(name = "deleteProgramId", description = "사용자 삭제 프로그램 Id(자체 회원탈퇴, 관리자 삭제, 개인정보 보존만료 등)", required = false, nullable = true, example = "admin_delete", maxLength = 20)
     @Size(max = 20)
+    @JsonProperty("deleteProgramId")
     private String deleteProgramId;
+
+    /**
+     * UsersRequestDTO 기본생성자
+     */
+    public UsersRequestDTO() {
+    }
 }
--- src/main/java/com/karrot/global/common/ResponseDTOWrapper.java
+++ src/main/java/com/karrot/global/common/ResponseDTOWrapper.java
@@ -19,28 +19,28 @@ public class ResponseDTOWrapper<T> {
      * data
      * 실제 응답 데이터
      */
-    @Schema(name = "data", description = "실데이터", required = false, nullable = true)
+    @Schema(name = "data", description = "실데이터, 제네릭 타입의 리스트. 데이터가 0건이어도, 1건이어도 리스트에 담음.", required = true, nullable = false, example = "[{\"id\":1,\"name\":\"example1\"},{\"id\":2,\"name\":\"example2\"}]")
     private List<T> data;
 
     /**
      * responseMessage
      * 사용자에게 표출되는 응답 메시지
      */
-    @Schema(name = "responseMessage", description = "사용자 메시지", required = true, nullable = false)
+    @Schema(name = "responseMessage", description = "사용자 메시지", required = true, nullable = false, example = "Create User Succeess")
     private String responseMessage;
 
     /**
      * responseStatus
      * 사용자에게 표출되는 응답코드
      */
-    @Schema(name = "responseStatus", description = "사용자 응답코드", required = true, nullable = false)
+    @Schema(name = "responseStatus", description = "사용자 응답코드 ResponseStatusEnum", required = true, nullable = false, example = "100")
     private ResponseStatusEnum responseStatus;
 
     /**
      * originalStatus
      * 사용자에게 노출되지 않는 실제 HTTP 응답코드
      */
-    @Schema(name = "originalStatus", description = "원본 응답코드", required = true, nullable = false)
+    @Schema(name = "originalStatus", description = "원본 응답코드 HttpStatus Enum", required = true, nullable = false, example = "200")
     private HttpStatus originalStatus;
 
     /**
